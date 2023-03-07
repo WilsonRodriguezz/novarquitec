ea56143e105f7cebe47be0ce4d57bbdbc71d6f8a
wilsonRodriguezz01 <whrodriguezz04@gmail.com>
2023-03-07 17:42:10 -0500

    About Section and Services Section Implementation

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -40,3 +40,32 @@ if (typed) {
         backDelay: 2000
     });
 }
+
+//Scroll active links
+/*const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+    })
+}
+
+scrollActive();
+window.addEventListener('scroll', scrollActive);*/
+
+//Scroll active links
+
+/*const li = document.querySelectorAll('.menu_link');
+const sec = document.querySelectorAll('section');
+
+function activeMenu() {
+    let len = sec.length;
+    while (--len && window.scrollY + 97 < sec[len].offsetTop) {
+        li.forEach(ltx => ltx.classList.remove('active-link'));
+        li[len].classList.add('active-link');
+    }
+
+}
+activeMenu();
+window.addEventListener('scroll', activeMenu);*/
